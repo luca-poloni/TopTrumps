f797deeb03b2336465e4b5ea9619f2c724c2701c
Luca Vilches Poloni <lucapoloni.main@outlook.com>
2023-12-08 15:35:50 -0300

    test: some domain tests

---
+++ tests/Domain.UnitTests/Entities/PlayerEntityTest.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace Domain.UnitTests.Entities
+{
+    public class PlayerEntityTest
+    {
+        [Fact]
+        public void GiveCard_Should_NotThrowException()
+        {
+            #region Arrange
+            var cardName = "Card One";
+            var playerMock = new Mock<PlayerEntity>();
+            var cardPlayers = new List<CardPlayerEntity> { new() { Card = new() { Name = cardName } } };
+            playerMock.Setup(x => x.CardPlayers).Returns(cardPlayers);
+            #endregion
+
+            #region Action
+            var action = () => { playerMock.Object.GiveCard(cardName); };
+            #endregion
+
+            #region Assert
+            action.Should().NotThrow<CardNotFoundException>(); 
+            #endregion
+        }
+
+        [Fact]
+        public void GiveCard_Should_ThrowException()
+        {
+            #region Arrange
+            var playerMock = new Mock<PlayerEntity>();
+            var cardPlayers = new List<CardPlayerEntity> { new() { Card = new() { Name = "Card One" } } };
+            playerMock.Setup(x => x.CardPlayers).Returns(cardPlayers);
+            #endregion
+
+            #region Action
+            var action = () => { playerMock.Object.GiveCard("Card Two"); };
+            #endregion
+
+            #region Assert
+            action.Should().Throw<CardNotFoundException>();
+            #endregion
+        }
+    }
+}
