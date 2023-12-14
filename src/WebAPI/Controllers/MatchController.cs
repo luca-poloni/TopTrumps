f40117f386aee95b3406215701bc9c3e3bfef1dd
Luca Poloni <luca.poloni@moveedu.com.br>
2023-12-14 11:50:14 -0300

    feat: add 'Match' controller

---
+++ src/WebAPI/Controllers/MatchController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Handlers.Match.Commands.CreateMatch;
+using Application.Handlers.Match.Commands.UpdateMatch;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("Match")]
+    [ApiController]
+    public class MatchController(IMediator mediator)
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<ActionResult<CreateMatchCommandResponse>> Create([FromQuery] CreateMatchCommandRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+
+        [Route("Update")]
+        [HttpPost]
+        public async Task<ActionResult<UpdateMatchCommandResponse>> Update([FromQuery] UpdateMatchCommandRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
