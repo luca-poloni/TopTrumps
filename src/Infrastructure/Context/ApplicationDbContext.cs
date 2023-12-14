15d1dbefe7c43d5ae34218ed757d8af6ba627424
Luca Poloni <luca.poloni@moveedu.com.br>
2023-12-14 13:31:12 -0300

    chore: remove unecessary using in 'ApplicationDbContext'

--- src/Infrastructure/Context/ApplicationDbContext.cs
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Common.Interfaces;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System.Reflection;
 
 namespace Infrastructure.Context
