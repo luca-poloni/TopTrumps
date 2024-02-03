63b810277abcd316212eed1594f75f844616bb45
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:25:09 -0300

    feat: add cardDeck domain module

---
+++ src/Domain/CardDeck/CardDeckEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+
+namespace Domain.CardDeck
+{
+    public sealed class CardDeckEntity(uint cardId, uint playerId) : BaseEntity<uint>
+    {
+        public uint CardId { get; private set; } = cardId;
+        public uint PlayerId { get; private set; } = playerId;
+    }
+}
---
+++ src/Domain/CardDeck/ICardDeckRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.CardDeck
+{
+    public interface ICardDeckRepository
+    {
+        Task<CardDeckEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<CardDeckEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
