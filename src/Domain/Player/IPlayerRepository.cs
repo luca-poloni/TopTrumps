e4f6237f384a54010b63d3295244103aa1dd7aec
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:14:15 -0300

    feat: add player domain module

---
+++ src/Domain/Player/IPlayerRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Player
+{
+    public interface IPlayerRepository
+    {
+        Task<PlayerEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<PlayerEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Domain/Player/PlayerEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+
+namespace Domain.Player
+{
+    public class PlayerEntity(uint matchId, string name) : BaseEntity<uint>
+    {
+        public uint MatchId { get; private set; } = matchId;
+        public string Name { get; private set; } = name;
+    }
+}
