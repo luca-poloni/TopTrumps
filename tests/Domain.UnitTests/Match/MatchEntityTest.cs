5d95339a5dac866daf244bc0f6225cf1deee73ad
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 16:21:45 -0300

    test: add match entity tests

---
+++ tests/Domain.UnitTests/Match/MatchEntityTest.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Game;
+using Domain.Match;
+using Domain.Player;
+using Domain.Round;
+using FluentAssertions;
+using Moq;
+
+namespace Domain.UnitTests.Match
+{
+    public class MatchEntityTest
+    {
+        [Fact]
+        public void Start_Should_NotThrowMatchIsFinishException()
+        {
+            #region Arrange
+            var match = new MatchEntity(gameId: It.IsAny<uint>());
+            #endregion
+
+            #region Action
+            var action = match.Start;
+            #endregion
+
+            #region Assert
+            action.Should().NotThrow<MatchIsFinishException>();
+            #endregion
+        }
+
+        [Fact]
+        public void Start_Should_ThrowMatchIsFinishException()
+        {
+            #region Arrange
+            var match = new MatchEntity(
+                gameId: It.IsAny<uint>(), 
+                isFinish: true, 
+                game: It.IsAny<GameEntity>(), 
+                players: It.IsAny<List<PlayerEntity>>(), 
+                rounds: It.IsAny<List<RoundEntity>>());
+            #endregion
+
+            #region Action
+            var action = match.Start;
+            #endregion
+
+            #region Assert
+            action.Should().Throw<MatchIsFinishException>();
+            #endregion
+        }
+    }
+}
