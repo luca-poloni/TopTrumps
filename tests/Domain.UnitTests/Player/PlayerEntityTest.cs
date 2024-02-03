1c7752459f0d78dfd11397eace980c7d26e2b724
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 15:44:55 -0300

    test: add player entity tests

---
+++ tests/Domain.UnitTests/Player/PlayerEntityTest.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Match;
+using Domain.Player;
+using FluentAssertions;
+using Moq;
+
+namespace Domain.UnitTests.Player
+{
+    public class PlayerEntityTest
+    {
+        [Fact]
+        public void IsAvailable_Should_True()
+        {
+            #region Arrange
+            var playerMock = new PlayerEntity(
+                 matchId: It.IsAny<uint>(),
+                 name: It.IsAny<string>(),
+                 match: It.IsAny<MatchEntity>(),
+                 cardDecks: [new(cardId: It.IsAny<uint>(), playerId: It.IsAny<uint>())]);
+
+            #endregion
+
+            #region Action
+            var isAvailable = playerMock.IsAvailable();
+            #endregion
+
+            #region Assert
+            isAvailable.Should().BeTrue();
+            #endregion
+        }
+
+        [Fact]
+        public void IsAvailable_Should_False()
+        {
+            #region Arrange
+            var playerMock = new PlayerEntity(
+                matchId: It.IsAny<uint>(),
+                name: It.IsAny<string>(),
+                match: It.IsAny<MatchEntity>(),
+                cardDecks: []);
+            #endregion
+
+            #region Action
+            var isAvailable = playerMock.IsAvailable();
+            #endregion
+
+            #region Assert
+            isAvailable.Should().BeFalse();
+            #endregion
+        }
+    }
+}
