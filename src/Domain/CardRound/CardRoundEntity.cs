284b65e59d821410065194d6926fddec24a04000
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:27:22 -0300

    feat: add cardRound domain module

---
+++ src/Domain/CardRound/CardRoundEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+
+namespace Domain.CardRound
+{
+    public sealed class CardRoundEntity(uint cardPlayerId, uint roundId) : BaseEntity<uint>
+    {
+        public uint CardPlayerId { get; private set; } = cardPlayerId;
+        public uint RoundId { get; private set; } = roundId;
+    }
+}
---
+++ src/Domain/CardRound/ICardRoundRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.CardRound
+{
+    public interface ICardRoundRepository
+    {
+        Task<CardRoundEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<CardRoundEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
