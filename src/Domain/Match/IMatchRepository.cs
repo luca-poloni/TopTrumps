0bf19474e6d683a24b2d74cf7faab23fea6983ad
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:30:15 -0300

    feat: add match domain module

---
+++ src/Domain/Match/IMatchRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Match
+{
+    public interface IMatchRepository
+    {
+        Task<MatchEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<MatchEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Domain/Match/MatchEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+
+namespace Domain.Match
+{
+    public sealed class MatchEntity(uint gameId) : BaseEntity<uint>
+    {
+        public uint GameId { get; private set; } = gameId;
+        public bool IsFinish { get; private set; }
+    }
+}
