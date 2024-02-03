ef79f79958038d8edcd8bcd35fe17fce9f1dd6f4
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:40:09 -0300

    feat: add power domain module

---
+++ src/Domain/Power/IPowerRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Power
+{
+    public interface IPowerRepository
+    {
+        Task<PowerEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<PowerEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Domain/Power/PowerEntity.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Core;
+
+namespace Domain.Power
+{
+    public sealed class PowerEntity(uint cardId, uint featureId, uint value) : BaseEntity<uint>
+    {
+        public uint CardId { get; } = cardId;
+        public uint FeatureId { get; } = featureId;
+        public uint Value { get; private set; } = value;
+    }
+}
