c363fd7393f9c32fdbe133b0af3ca3503576d58b
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:12:13 -0300

    feat: add card domain module

---
+++ src/Domain/Card/CardEntity.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core;
+using Domain.Game;
+
+namespace Domain.Card
+{
+    public class CardEntity(uint gameId, string name, byte[] image, bool isTopTrumps) : BaseEntity<uint>
+    {
+        public uint GameId { get; private set; } = gameId;
+        public string Name { get; private set; } = name;
+        public byte[] Image { get; private set; } = image;
+        public bool IsTopTrumps { get; private set; } = isTopTrumps;
+        public GameEntity Game { get; } = null!;
+    }
+}
---
+++ src/Domain/Card/ICardRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Card
+{
+    public interface IGameRepository
+    {
+        Task<CardEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<CardEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
