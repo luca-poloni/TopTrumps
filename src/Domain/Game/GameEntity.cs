2c4ccbe57612dab647119b48e31df2bc8a28c343
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-03 03:07:38 -0300

    feat: add game domain module

---
+++ src/Domain/Game/GameEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+
+namespace Domain.Game
+{
+    public class GameEntity(string name, string description) : BaseEntity<uint>
+    {
+        public string Name { get; private set; } = name;
+        public string Description { get; private set; } = description;
+    }
+}
---
+++ src/Domain/Game/IGameRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Game
+{
+    public interface IGameRepository
+    {
+        Task<GameEntity> GetById(uint id, CancellationToken cancellationToken);
+        Task<GameEntity> GetAll(CancellationToken cancellationToken);
+    }
+}
