63bf8ca3c768d79d61dd8f691292483a3d218e41
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-10 23:31:20 -0300

    test: add game entity tests

---
+++ tests/Domain.UnitTests/Card/CardEntityTest.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Card;
+using Domain.CardDeck;
+using Domain.Game;
+using Domain.Power;
+using FluentAssertions;
+using Moq;
+
+namespace Domain.UnitTests.Card
+{
+    public class CardEntityTest
+    {
+        [Fact]
+        public void WinnerPowerByValue_Should_ValidPower()
+        {
+            #region Arrange
+            var card = new CardEntity(
+                gameId: It.IsAny<uint>(), 
+                name: It.IsAny<string>(), 
+                image: It.IsAny<byte[]>(), 
+                isTopTrumps: It.IsAny<bool>(), 
+                game: It.IsAny<GameEntity>(), 
+                cardDecks: It.IsAny<List<CardDeckEntity>>(), 
+                powers: [new(cardId: It.IsAny<uint>(), featureId: It.IsAny<uint>(), value: 100)]);
+            #endregion
+
+            #region Action
+            var winnerPowerByValue = card.WinnerPowerByValue(100);
+            #endregion
+
+            #region Assert
+            winnerPowerByValue.Should().BeOfType<PowerEntity>();
+            #endregion
+        }
+
+        [Fact]
+        public void WinnerPowerByValue_Should_NoPower()
+        {
+            #region Arrange
+            var card = new CardEntity(
+                gameId: It.IsAny<uint>(),
+                name: It.IsAny<string>(),
+                image: It.IsAny<byte[]>(),
+                isTopTrumps: It.IsAny<bool>(),
+                game: It.IsAny<GameEntity>(),
+                cardDecks: It.IsAny<List<CardDeckEntity>>(),
+                powers: [new(cardId: It.IsAny<uint>(), featureId: It.IsAny<uint>(), value: 100)]);
+            #endregion
+
+            #region Action
+            var winnerPowerByValue = card.WinnerPowerByValue(50);
+            #endregion
+
+            #region Assert
+            winnerPowerByValue.Should().BeNull();
+            #endregion
+        }
+    }
+}
