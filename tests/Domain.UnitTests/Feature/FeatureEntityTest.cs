4f703c699eb866bc6d878b3d5fbf2b3e90764949
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-02-10 23:32:15 -0300

    test: add feature entity tests

---
+++ tests/Domain.UnitTests/Feature/FeatureEntityTest.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Card;
+using Domain.Feature;
+using Domain.Game;
+using Domain.Power;
+using FluentAssertions;
+using Moq;
+
+namespace Domain.UnitTests.Feature
+{
+    public class FeatureEntityTest
+    {
+        [Fact]
+        public void PowerByCard_Should_ValidPower()
+        {
+            #region Arrange
+            var card = new CardEntity(
+                gameId: It.IsAny<uint>(), 
+                name: It.IsAny<string>(), 
+                image: It.IsAny<byte[]>(), 
+                isTopTrumps: It.IsAny<bool>());
+
+            var feature = new FeatureEntity(
+                gameId: It.IsAny<uint>(),
+                name: It.IsAny<string>(),
+                game: It.IsAny<GameEntity>(),
+                powers: [new(
+                    cardId: It.IsAny<uint>(), 
+                    featureId: It.IsAny<uint>(), 
+                    value: It.IsAny<uint>(), 
+                    card: card, 
+                    feature: It.IsAny<FeatureEntity>())]);
+            #endregion
+
+            #region Action
+            var winnerPowerByValue = feature.PowerByCard(card);
+            #endregion
+
+            #region Assert
+            winnerPowerByValue.Should().BeOfType<PowerEntity>();
+            #endregion
+        }
+
+        [Fact]
+        public void PowerByCard_Should_NoPower()
+        {
+            #region Arrange
+            var card = new CardEntity(
+                gameId: It.IsAny<uint>(),
+                name: It.IsAny<string>(),
+                image: It.IsAny<byte[]>(),
+                isTopTrumps: It.IsAny<bool>());
+
+            var feature = new FeatureEntity(
+                gameId: It.IsAny<uint>(),
+                name: It.IsAny<string>(),
+                game: It.IsAny<GameEntity>(),
+                powers: [new(
+                    cardId: It.IsAny<uint>(),
+                    featureId: It.IsAny<uint>(),
+                    value: It.IsAny<uint>(),
+                    card: It.IsAny<CardEntity>(),
+                    feature: It.IsAny<FeatureEntity>())]);
+            #endregion
+
+            #region Action
+            var winnerPowerByValue = feature.PowerByCard(card);
+            #endregion
+
+            #region Assert
+            winnerPowerByValue.Should().BeNull();
+            #endregion
+        }
+    }
+}
