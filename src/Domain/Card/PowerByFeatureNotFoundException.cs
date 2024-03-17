5fce00218c2bcc3366aa01bf86e5e0bbcb8c8bce
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 13:19:58 -0300

    chore: remove unsed exception class

--- src/Domain/Card/PowerByFeatureNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Core;
-
-namespace Domain.Card
-{
-    public class PowerByFeatureNotFoundException : DomainBaseException
-    {
-        public PowerByFeatureNotFoundException() : base("Power by feature not found.") { }
-    }
-}
