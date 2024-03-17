18c9efb81e76db1267e91be88a8407fcb3bb7eb6
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 14:00:53 -0300

    feat: add validation behavior

---
+++ src/Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace Application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                if (context != default)
+                {
+                    var validationResults = await ValidationResults(context, cancellationToken);
+
+                    if (validationResults != default)
+                    {
+                        var failures = Failures(validationResults);
+
+                        if (failures != default && failures.Count > 0)
+                        {
+                            var message = FailureMessage(failures);
+
+                            if (!string.IsNullOrWhiteSpace(message))
+                                throw new ArgumentException(message);
+                        }
+                    }
+
+                }
+            }
+
+            return await next();
+        }
+
+        private async Task<ValidationResult[]> ValidationResults(ValidationContext<TRequest> context, CancellationToken cancellationToken)
+        {
+            return await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
+        }
+
+        private static List<ValidationFailure> Failures(ValidationResult[] validationResults)
+        {
+            return validationResults
+                .Where(r => r.Errors.Count > 0)
+                .SelectMany(r => r.Errors)
+                .ToList();
+        }
+
+        private static string FailureMessage(List<ValidationFailure> failures)
+        {
+            var message = string.Empty;
+
+            foreach (var failure in failures)
+                message += failure.ErrorMessage + Environment.NewLine;
+
+            return message;
+        }
+    }
+}
