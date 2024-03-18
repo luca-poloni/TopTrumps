f87688da71254ecb03f56d5f04396efdf65c1253
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 21:20:05 -0300

    feat: add application dbContenxt interface

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Card;
+using Domain.Feature;
+using Domain.Game;
+using Domain.Match;
+using Domain.Player;
+using Domain.Power;
+using Domain.Round;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<CardEntity> Cards { get; }
+        public DbSet<FeatureEntity> Features { get; }
+        public DbSet<GameEntity> Games { get; }
+        public DbSet<MatchEntity> Matches { get; }
+        public DbSet<MatchEntity.MatchCardEntity> MatchCards { get; }
+        public DbSet<PlayerEntity> Players { get; }
+        public DbSet<PlayerEntity.PlayerCardEntity> PlayerCards { get; }
+        public DbSet<PowerEntity> Powers { get; }
+        public DbSet<RoundEntity> Rounds { get; }
+        public DbSet<RoundEntity.RoundCardEntity> RoundCards { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
