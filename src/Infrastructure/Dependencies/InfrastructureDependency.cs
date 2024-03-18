1832c21ebc5c8ff6f738bce870dab8e4cac1dbf2
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 22:51:36 -0300

    feat: add infrastructure dependencies

---
+++ src/Infrastructure/Dependencies/InfrastructureDependency.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Dependencies
+{
+    public static class InfrastructureDependency
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddContext(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionStringName = "DefaultConnection";
+            var connectionString = configuration.GetConnectionString(connectionStringName);
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"Connection string '{connectionStringName}' not found.");
+
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>((sp, options) =>
+            {
+                options
+                .UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
