56dd1b908604335825b0a13e66d46d695c2ae9d1
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 21:23:43 -0300

    feat: add application dependencies file

---
+++ src/Application/Common/Dependencies/ApplicationDependency.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.Common.Dependencies
+{
+    public static class ApplicationDependency
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddHandler();
+            services.AddValidator();
+
+            return services;
+        }
+
+        private static IServiceCollection AddHandler(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddValidator(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
