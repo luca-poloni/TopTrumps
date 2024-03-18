72fee1a2201d9add3f03f0bfb52ad3d7cc4c2e0c
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-03-17 22:49:26 -0300

    feat: add application dbContext in infrastrucuture layer

---
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Domain.Card;
+using Domain.Feature;
+using Domain.Game;
+using Domain.Match;
+using Domain.Player;
+using Domain.Power;
+using Domain.Round;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IApplicationDbContext
+    {
+        public DbSet<CardEntity> Cards => Set<CardEntity>();
+        public DbSet<FeatureEntity> Features => Set<FeatureEntity>();
+        public DbSet<GameEntity> Games => Set<GameEntity>();
+        public DbSet<MatchEntity> Matches => Set<MatchEntity>();
+        public DbSet<MatchEntity.MatchCardEntity> MatchCards => Set<MatchEntity.MatchCardEntity>();
+        public DbSet<PlayerEntity> Players => Set<PlayerEntity>();
+        public DbSet<PlayerEntity.PlayerCardEntity> PlayerCards => Set<PlayerEntity.PlayerCardEntity>();
+        public DbSet<PowerEntity> Powers => Set<PowerEntity>();
+        public DbSet<RoundEntity> Rounds => Set<RoundEntity>();
+        public DbSet<RoundEntity.RoundCardEntity> RoundCards => Set<RoundEntity.RoundCardEntity>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
