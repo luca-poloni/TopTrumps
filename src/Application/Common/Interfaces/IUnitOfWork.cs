5ab922c995e3f21dcb3d7362aa0df223995be5ac
Luca Vilches Poloni <lucapoloni.main@gmail.com>
2024-04-06 12:10:18 -0300

    feat: Add the 'CreateCard' endpoint
    #2

--- src/Application/Common/Interfaces/IUnitOfWork.cs
+++ src/Application/Common/Interfaces/IUnitOfWork.cs
@@ -1,10 +1,13 @@
-﻿using Domain.Core.Game;
+﻿using Domain.Core.Card;
+using Domain.Core.Game;
 
 namespace Application.Common.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
+        ICardRepository CardRepository { get; }
         IGameRepository GameRepository { get; }
+
         Task<int> CommitAsync(CancellationToken cancellationToken);
     }
 }
